f6858e0ee1066b017bff2747c36845942dfe3374
iril <iril@kitabisa.com>
2023-10-04 10:25:09 +0700

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Iril on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
