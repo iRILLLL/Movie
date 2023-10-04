b5f2e158bd1029eca7d9070a8586f2e7f5b1b2ba
iril <iril@kitabisa.com>
2023-10-04 17:04:19 +0700

    Create API Client

--- features/Sources/TMDBCore/TMDBClient.swift
+++ features/Sources/TMDBCore/TMDBClient.swift
@@ -1,1 +1,31 @@
 import Dependencies
+import XCTestDynamicOverlay
+
+public struct TMDBClient {
+    
+    public var genreList: @Sendable () async throws -> [Genre]
+    
+    public init(
+        genreList: @escaping @Sendable () async throws -> [Genre]
+    ) {
+        self.genreList = genreList
+    }
+}
+
+public enum TMDBClientKey: TestDependencyKey {
+    
+    public static var previewValue = TMDBClient(
+        genreList: unimplemented("\(Self.self).genreList")
+    )
+    
+    public static var testValue = TMDBClient(
+        genreList: unimplemented("\(Self.self).genreList")
+    )
+}
+
+extension DependencyValues {
+    public var tmdbClient: TMDBClient {
+        get { self[TMDBClientKey.self] }
+        set { self[TMDBClientKey.self] = newValue }
+    }
+}
--- features/Sources/TMDBCoreLive/TMDBClientLive.swift
+++ features/Sources/TMDBCoreLive/TMDBClientLive.swift
@@ -1,1 +1,28 @@
+import Foundation
 import Dependencies
+import TMDBCore
+import Get
+
+extension TMDBClientKey: DependencyKey {
+    
+    public static let liveValue: TMDBClient = .init(
+        genreList: {
+            let path = "/genre/tv/list"
+            let request = Request<GenreListResponse>(
+                path: path,
+                query: [("language", "en")]
+            )
+            let client = APIClient(baseURL: URL(string: "https://api.themoviedb.org/3")) {
+                $0.delegate = ClientDelegate()
+            }
+            let response = try await client.send(request).value
+            return response.genres.map(Genre.init)
+        }
+    )
+}
+
+final class ClientDelegate: APIClientDelegate {
+    func client(_ client: APIClient, willSendRequest request: inout URLRequest) async throws {
+        request.setValue("Bearer \(Secrets.Keys.TMDB.accessToken)", forHTTPHeaderField: "Authorization")
+    }
+}
